abf3a8775c7001905758077eb0a5b4b28a7b0ea3
phan.nguyen <phan.nguyen@mail.utoronto.ca>
2025-12-10 16:29:55 -0800

    comments

--- Models/MLPMusicGen.py
+++ Models/MLPMusicGen.py
@@ -103,7 +103,7 @@ class MLPMusicGen(MLPNetwork):
     def predict(self, x):
         """
         Override predict so that:
-        - x can be a nested list (list of lists, etc.)
+        - x can be a nested list (list of lists, etc.), because it is aone hot encoded list of notes
         - flatten it automatically before calling parent.predict()
         """
         # Convert to numpy array so we can flatten cleanly
